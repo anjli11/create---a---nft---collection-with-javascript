40c226a03a1b351e1f76dd5db7989db2b92fda28
anjli11 <137086343+anjli11@users.noreply.github.com>
2023-06-27 12:18:36 +0530

    Update playground.js

--- playground.js
+++ playground.js
@@ -1,29 +1,37 @@
-// Define the NFT object
-const NFT = {
-    name: "My Awesome NFT",
-    eyeColor: "blue",
-    shirtType: "t-shirt",
-    bling: true,
-  };
-  
-  // Variable to store all NFTs
-  const myNFTs = [];
-  
-  // Function to print NFT details
-  function printNFTDetails(nft) {
-    console.log("NFT Name: " + nft.name);
-    console.log("Eye Color: " + nft.eyeColor);
-    console.log("Shirt Type: " + nft.shirtType);
-    console.log("Bling: " + (nft.bling ? "Yes" : "No"));
-    console.log("----------------------");
+let NFTs=[]
+
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata, 
+// and store it in the variable above.
+function mintNFT(name, Color, shirtType) {
+    const nft = {
+      name: name,
+      Color: Color,
+      shirtType: shirtType,
+    };
+    NFTs.push(nft);
+  }
+
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
+function listNFTs() {
+    for (let i = 0; i < NFTs.length; i++) {
+      console.log("Name: " + NFTs[i].name);
+      console.log("Eye Color: " + NFTs[i].Color);
+      console.log("Shirt Type: " + NFTs[i].shirtType);
+      console.log("--------------------------");
+    }
+  }
+
+// print the total number of NFTs we have minted to the console
+function getTotalSupply() {
+    return NFTs.length;
   }
   
-  // Add the NFT to the collection
-  myNFTs.push(NFT);
-  
-  // Print the details of all NFTs in the collection
-  for (let i = 0; i < myNFTs.length; i++) {
-    const nft = myNFTs[i];
-    printNFTDetails(nft);
-  }
-  
+
+// call your functions below this line
+mintNFT("NFT 1", "Blue", "T-shirt", );
+mintNFT("NFT 2", "Green", "Hoodie", );
+mintNFT("NFT 3","Red","Shirt",);
+listNFTs();
+console.log("Total Supply: " + getTotalSupply());
